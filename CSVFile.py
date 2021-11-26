faf7201909f542a445c9d337caea177be90ae026
Alberto Facchin <77962285+FacchinAlberto@users.noreply.github.com>
2021-11-26 16:18:13 +0000

    Added NumericalCSVFile class to CSVFile.py

--- CSVFile.py
+++ CSVFile.py
@@ -4,20 +4,33 @@ class CSVFile():
         self.file = file
     def get_data(self):
         my_list = []
-        my_file = open(self.file, 'r')
-        for line in my_file:
-            elem = line.split(',')
-            if elem[0] != 'Date':
-                my_list.append(elem)
-        
-        my_file.close()
-        return my_list;
+        try:
+            my_file = open(self.file, 'r')
+            for line in my_file:
+                elem = line.rstrip().split(',')
+                if elem[0] != 'Date':
+                    my_list.append(elem)
+            
+            my_file.close()
+            return my_list;
+        except Exception as e:
+            print('ERROR: Impossibile aprire file "{}"'.format(self.file))
 
-mia_lista = []
-obj = CSVFile('Vendite shampoo', 'shampoo_sales.csv')
+class NumericalCSVFile(CSVFile):
+    def get_data(self):
+        my_list = super().get_data()
+        for item in my_list:
+            item[1] = float(item[1])
+        return my_list
+
+obj = CSVFile(input('Nome file: '), input('Percorso file: '))
 mia_lista = obj.get_data()
 print('Nome file: {}'.format(obj.name))
 print('Contenuto: ')
 for x in mia_lista:
-    #print("['{}, {}']".format(x[0], x[1]))
+    print(x)
+numobj = NumericalCSVFile(obj.name, obj.file)
+mia_lista = numobj.get_data()
+print('\nContenuto con conversione in float: ')
+for x in mia_lista:
     print(x)
