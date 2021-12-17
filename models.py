9262e579efc52ed209384f52ea70ba5cdd898ec0
Alberto Facchin <77962285+FacchinAlberto@users.noreply.github.com>
2021-12-17 14:18:18 +0000

    finished: models.py

--- models.py
+++ models.py
@@ -29,47 +29,107 @@ class Model():
     def predict(self, data):
         raise NotImplementedError("Metodo non implementato")        
 
-class IncrementedModel(Model):
+class IncrementModel(Model):
     def check_data(self, data):
         if len(data)<2:
             return 'Impossibile fare previsione, numero di dati insufficienti'
         else:
             return True
     #def preditct(self, data):  
+    def __str__(self):
+        return 'IncrementModel'
 
-class FitIncrementModel(IncrementedModel):
-    def fit(self, data):
+    def compute_avg_increment(self, data):
         if self.check_data(data):
             inc = 0.0
-            #controllo se ci sono errori nel file come stringhe al posto di float
             for i in range(0, len(data)-1):
                 inc += float(data[i+1]) - float(data[i])    
-            global_avg_increment = inc/len(data)
-            return global_avg_increment
+            avg_increment = inc/len(data)
+            return avg_increment
         else:
             print(self.check_data(data))
     
-    def predict(self, data, global_avg_increment):
-        prediction = float(data[len(data)-1]) + global_avg_increment
-        data.append(prediction)
-        return data
+    def predict(self, predict_data):
+        avg_increment = self.compute_avg_increment(predict_data)
+        return float(predict_data[-1]) + avg_increment
+
+class FitIncrementModel(IncrementModel):
+    def __str__(self):
+        return 'FitIncrementModel'
+
+    def fit(self, fit_data):
+        # Calcolo l'incremento medio sui dati di fit
+        self.global_avg_increment = self.compute_avg_increment(fit_data)
+
+    def predict(self, predict_data):
+        # Chiamo la predict della classe genitore "IncrementModel"
+        parent_prediction = super().predict(predict_data)
+        # Sottraggo l'ultimo valore alla predizione del genitore
+        # cosi' da avre l'incremento "originale"
+        parent_predict_increment = parent_prediction - float(predict_data[-1])
+        # Ora medio l'incremento del fit con quello della predict
+        prediction_increment = (self.global_avg_increment + parent_predict_increment) / 2
+        # E lo ri-sommo all'ultimo elemento
+        prediction = float(predict_data[-1]) + prediction_increment
+        return prediction
         
+## MAIN ##
 csvobj = CSVFile('Vendite shampoo', 'shampoo_sales.csv')
 my_list = csvobj.get_data()
 data = []
-values = []
 for item in my_list:
     data.append(item[1])
+
+"""
 fitobj = FitIncrementModel()
+fitobj.fit(data)
+
 
 #numero di mesi di cui voglio predire i valori
 number_of_months = 12
 
 for i in range(1, number_of_months+1):
-    global_avg_increment = fitobj.fit(data)
-    values = fitobj.predict(data, global_avg_increment)
-    csvobj.write('prediction '+str(i)+','+str(round(values[len(values)-1], 2)))
-    
-pyplot.plot(values, color='tab:blue')
-#pyplot.plot(values, color='tab:red')
+    prediction = fitobj.predict(data)
+    print(prediction)
+    data.append(prediction)
+    csvobj.write('prediction '+str(i)+','+str(round(data[len(values)-1], 2)))
+"""
+
+number_of_months = 12
+cutoff_month = len(data) - number_of_months
+
+real_data = []
+test_data = []
+
+for i in range(len(data)):
+    if i < cutoff_month:
+        test_data.append(data[i])
+    else:
+        real_data.append(data[i])
+
+incobj = IncrementModel()
+fitobj = FitIncrementModel()
+fitobj.fit(test_data)
+predictions = []
+fit_predictions = []
+fit_error = 0
+error = 0
+
+for i in range(number_of_months):
+    predict_data = data[cutoff_month+i-3-1:cutoff_month+i-1]
+    prediction = incobj.predict(predict_data)
+    predictions.append(prediction)
+    prediction_fit = fitobj.predict(predict_data)
+    fit_predictions.append(prediction_fit)
+    print('"{}" (real) vs "{}" (fit pred) vs "{}" (pred)\n'.format(real_data[i], round(prediction_fit, 1), round(prediction,1 )))
+    fit_error += abs(prediction_fit - float(real_data[i]))
+    error += abs(prediction - float(real_data[i]))
+
+fit_error /= number_of_months
+error /= number_of_months
+print('Fit error: {}\nNon fit error: {}'.format(fit_error, error))
+
+pyplot.plot(test_data + predictions, color='tab:red')
+pyplot.plot(test_data + fit_predictions, color='tab:green')
+pyplot.plot(data, color='tab:blue')
 pyplot.show()
