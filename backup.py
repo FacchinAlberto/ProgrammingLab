2fa4618c172b718f3ae21bfa3f09e3eacd350c0d
Alberto Facchin <77962285+FacchinAlberto@users.noreply.github.com>
2022-02-08 13:52:49 +0000

    still finishing esame.py, added a backup.py for saving the last "checkpoint" of esame.py

--- backup.py
+++ backup.py
@@ -29,7 +29,7 @@ class CSVTimeSeriesFile():
                 else:
                     for line in my_file:
                         current_line += 1
-                        #controllo se la riga non è vuota e se contiene la virgola
+                        #controllo se la riga non è vuota
                         if line != '\n' and ',' in line:
                             elem = line.rstrip().split(',')
                             if elem[0] != 'date':
@@ -38,16 +38,13 @@ class CSVTimeSeriesFile():
                                 #controllo se è presente un duplicato
                                 if '-' not in elem[0]:
                                     print('ERROR: anomalia a riga {}'.format(current_line))
-                                #controllo se quella data è già presente
                                 elif elem[0].rstrip().split('-') in years_and_months:
                                     raise ExamException('ERROR: nel file è presente un duplicato di {}'.format(elem[0]))
                                 else:
                                     years_and_months.append(elem[0].rstrip().split('-'))
                                     #ogni elemento di years_and_months contiene una lista del tipo ['1953', '05']
-
-                                    #mi salvo la data corrente per poi vedere se il file è ordinato
                                     current_date = elem[0].rstrip().split('-')
-                                    
+                                    #controllo se c'è un timestamp non in ordine
                                     try:
                                         current_date[0] = int(current_date[0])
                                         current_date[1] = int(current_date[1])
@@ -56,10 +53,8 @@ class CSVTimeSeriesFile():
                                         converted_in_int = False
                                         print('ERROR: impossibile convertire {} in int'.format(current_date))
                                     if converted_in_int:
-                                        #controllo se c'è un timestamp non in ordine
                                         if current_date[0] <= last_date[0] and current_date[1] <= last_date[1]:
                                             raise ExamException('ERROR: elemento "{}" non ordinato in modo crescente'.format(elem[0]))
-                                            
                                         #controllo che il mese sia compreso tra 1 e 12
                                         elif int(current_date[1])<=0 or int(current_date[1])>12:
                                             raise ExamException('ERROR: elemento "{}" non rispetta i canoni'.format(elem[0]))
@@ -78,7 +73,8 @@ class CSVTimeSeriesFile():
                                                 my_list.append(elem)
                                         except Exception as e:
                                             print('ERROR: impossibile convertire valore paseggeri "{}" della riga {} in int'.format(elem[1], current_line))
-                                    
+                                
+                    #print(my_list[:4])      
                     return my_list;
                 my_file.close()                    
                 #except Exception as e:
@@ -97,7 +93,7 @@ def year_in_file(time_series, year):
         elem = str(item[0]) #prendo anno e mese insieme
         year_and_month = elem.rstrip().split('-')
         #in year_and_month viene messa una lista del tipo ['1953', '05']
-        list_of_years.append(int(year_and_month[0]))
+        list_of_years.append(int(year_and_month[0]))    #da verificare int()
     if (year in list_of_years):
         return True
     else:
@@ -114,7 +110,6 @@ def parameter_ok(parameter):
 def time_series_ok(time_series):
     if time_series is not None and isinstance(time_series, list):
         for item in time_series:
-            #controllo se c'è un elemento None o se non c'è la virgola o se nella data non è presente in trattino
             if item is None or ',' not in str(item) or '-' not in item[0]:
                 return False 
             else:
@@ -123,6 +118,7 @@ def time_series_ok(time_series):
         return False
     
 def compute_avg_monthly_difference(time_series, first_year, last_year):
+    years = []
     months = []
     passengers = []
     converted_in_int = True
@@ -134,9 +130,7 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
             converted_in_int = False
             print('ERROR: impossibile convertire first_year e/o last_year in int')
         if converted_in_int:
-            #controllo che time_series sia accettabile
             if time_series_ok(time_series):
-                #controllo che first_year e last_year siano compresi nel file
                 if year_in_file(time_series, first_year) and year_in_file(time_series, last_year):
 
                     #controllo se first_year è uguale da last_year
@@ -155,9 +149,14 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
                             year_and_month = elem.rstrip().split('-')
                             #in year_and_month viene messa una lista del tipo ['1953', '05']
                             if int(year_and_month[0]) >= first_year and int(year_and_month[0])<= last_year:
-                                #creo due liste, una per i mesi ed una per i corrispettivi valori dei passeggeri
+                                years.append(int(year_and_month[0]))
                                 months.append(int(year_and_month[1]))
                                 passengers.append(int(item[1]))
+                                #if len(my_dict[year_and_month[1]]) == 0:
+                                 #   my_dict[year_and_month[1]] = []
+                                #my_dict[year_and_month[1]].append(int(item[1]))
+                        
+
 
                         #inizializzo la lista delle variazioni medie con ogni elemento = 0, in modo da poter utilizzare successivamente il +=
                         avg_variation = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -167,33 +166,47 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
 
                         #mi serve per contare di quanti e di quali mesi ho la misurazione
                         freq_of_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                        #for i in range (0,12):
+                            
+
 
                         for i in range (0,12):
-                            #conto quante misurazioni ho per quel mese
                             freq_of_month[i] += months.count(i+1)
-                            #prev_value
-                            previous_value = 0
+                            prev_value = 0
                             for j in range(0, len(months)):
                                 if i+1 == months[j]:
-                                    #se è la prima volta che "incontro" quel mese mi salvo il valore dei passeggeri
-                                    if previous_value == 0:
-                                        previous_value = passengers[j]
-                                    #altrimenti faccio il valore attuale dei passeggeri per quel mese meno il valore precedente (sempre di quel mese) e poi aggiorno previous_value
+                                    if prev_value == 0:
+                                        prev_value = passengers[j]
                                     else:
-                                        avg_variation[i] += passengers[j] - previous_value
-                                        previous_value = passengers[j]
-                            #intervallo di due anni e meno di due misurazioni
-                            if diff == 1 and freq_of_month[i] < 2:
-                                avg_variation[i] = 0
-                            #intervallo di più di due anni e meno di due misurazioni
+                                        avg_variation[i] += passengers[j] - prev_value
+                                        prev_value = passengers[j]
+                            if diff == 1:
+                                if freq_of_month[i] < 2:
+                                    avg_variation[i] = 0
                             elif diff > 1 and freq_of_month[i] < 2:
                                 avg_variation[i] = 0
-                            #intervallo di più di due anni e ho almeno due misurazioni, ma comunque ne manca almeno una
                             elif diff > 1 and freq_of_month[i] <= diff:
-                                avg_variation[i] /= freq_of_month[i]-1
-                            #intervallo di n anni e ho n misurazioni
+                                avg_variation[i] /= diff-1
                             else:
                                 avg_variation[i] /= diff
+
+                        print(avg_variation)
+
+                        """
+                        for i in range(0, 12):
+                            for j in range(0, diff):
+                                #dovrei scrivere diff-1, ma visto che per es. 2021-2019 = 2, ed io devo considerare 3 anni (2019, 2020, 2021) poosso usare diff
+                                avg_variation[i] += passengers[(12*(j+1))+i]-passengers[(12*j)+i]   
+                                #esempio della prima iterazione del ciclo "esterno" (utilizzando 1949, 1950, 1951):
+                                # avg_variation[0] += passengers[12]-passengers[0]
+                                # avg_variation [0] += passengers[24]-passengers[12]
+
+                                #esempio della seconda iterazione del ciclo "esterno" (utilizzando 1949, 1950, 1951):
+                                # avg_variation[0] += passengers[13]-passengers[1]
+                                # avg_variation [0] += passengers[25]-passengers[13]
+                            avg_variation[i] /= diff
+                            #divido ogni elemento di avg_variation per il "numero di sottrazioni fatte"
+                        """
 
                         return avg_variation
                 else:
